3bc6f9924aef6187c8e6ac6016f35da3b2a41f96
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-25 18:40:16 -0800

    Modified: Add Numbers Using Size Method To Check Size

--- src/com/bridgelabz/BinaryMain.java
+++ src/com/bridgelabz/BinaryMain.java
@@ -8,6 +8,18 @@ public class BinaryMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(40);
+		myBinaryTree.add(22);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.size();
+
 	}
 
 }
--- src/com/bridgelabz/MyBinarySearchTree.java
+++ src/com/bridgelabz/MyBinarySearchTree.java
@@ -21,4 +21,19 @@ public class MyBinarySearchTree<K extends Comparable<K>> {
 		System.out.println(current);
 		return current;
 	}
+
+	public void size() {
+		int outputSize = sizeRecursive(root); // 3
+		System.out.println("Size is" + outputSize);
+	}
+
+	private Integer sizeRecursive(MyBinaryNode<K> root) {
+		Integer value;
+		if (root == null) {
+			value = 0;
+		} else {
+			value = sizeRecursive(root.left) + sizeRecursive(root.right) + 1;
+		}
+		return value;
+	}
 }
